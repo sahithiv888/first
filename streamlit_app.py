189f8e8137fa2b34c950f5e821ba8eb0e2b3537c
sahithiv888 <44580382+sahithiv888@users.noreply.github.com>
2023-05-02 21:08:17 -0400

    Update streamlit_app.py
    changed the items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title("My parents new healthy diner")
-streamlit.header("Indian Menu")
-streamlit.text("1.chicken biryani")
-streamlit.text("2.mutton curry")
-streamlit.text("3.vegetable biryani")
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
