553b27e9c0b4cf2a9e60810c3adcf22e7443b0e8
sahithiv888 <44580382+sahithiv888@users.noreply.github.com>
2023-05-02 21:06:49 -0400

    Update streamlit_app.py
    added header and some items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
 streamlit.title("My parents new healthy diner")
+streamlit.header("Indian Menu")
 streamlit.text("1.chicken biryani")
 streamlit.text("2.mutton curry")
+streamlit.text("3.vegetable biryani")
