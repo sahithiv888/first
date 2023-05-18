ba2df00cabe5cf510e18211a9ef94bfd7aa12637
sahithiv888 <44580382+sahithiv888@users.noreply.github.com>
2023-05-18 18:56:38 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
-#import pandas
+import pandas
 import snowflake.connector
-#import requests
+import requests
 from urllib.error import URLError
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.title("My Mom's new healthy diner")
